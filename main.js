286ffbe07a8bbc0eecac2d0d64dd5da91aa82261
Hiroki Kato <hirokikato1@gmail.com>
2024-04-05 14:29:42 -0400

    fixed main.js issue with main positioning

--- main.js
+++ main.js
@@ -2,44 +2,57 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+// Setup
+
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
+camera.position.setX(-3);
+
 renderer.render(scene, camera);
 
+// Torus
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({
-  color: 0x8bbccc,
-});
+const material = new THREE.MeshStandardMaterial({ color: 0x00abb3 });
 const torus = new THREE.Mesh(geometry, material);
+
 scene.add(torus);
+
+// Lights
 
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
+
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-//shows position of pointLight with position and wireframe
-const lightHelper = new THREE.PointLightHelper(pointLight);
-//grid-helper
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
-const controls = new OrbitControls(camera, renderer.domElement);
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 10, 10);
-  const material = new THREE.MeshStandardMaterial({ color: 0x4c6793 });
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
+
   const [x, y, z] = Array(3)
     .fill()
     .map(() => THREE.MathUtils.randFloatSpread(100));
@@ -47,20 +60,30 @@ function addStar() {
   star.position.set(x, y, z);
   scene.add(star);
 }
+
 Array(200).fill().forEach(addStar);
+
+// Background
 
 const spaceTexture = new THREE.TextureLoader().load("nightsky.png");
 scene.background = spaceTexture;
 
+// Avatar
+
 const hiroTexture = new THREE.TextureLoader().load("hiro.png");
+
 const hiro = new THREE.Mesh(
   new THREE.BoxGeometry(3, 3, 3),
   new THREE.MeshBasicMaterial({ map: hiroTexture })
 );
+
 scene.add(hiro);
+
+// Moon
 
 const moonTexture = new THREE.TextureLoader().load("moon.jpg");
 const normalTexture = new THREE.TextureLoader().load("normal.jpg");
+
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
@@ -68,25 +91,48 @@ const moon = new THREE.Mesh(
     normalMap: normalTexture,
   })
 );
+
+scene.add(moon);
+
 moon.position.z = 30;
 moon.position.setX(-10);
-scene.add(moon);
+
+hiro.position.z = -5;
+hiro.position.x = 2;
+
+// Scroll Animation
 
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
   moon.rotation.x += 0.05;
-}
-document.body.onscroll = moveCamera;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
 
-//rerender screen to see it with recursive function with an infinite loop so you do not have to call the render method repeatedly
-// renderer.render(scene, camera)
+  hiro.rotation.y += 0.01;
+  hiro.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
 function animate() {
   requestAnimationFrame(animate);
-  //every time screen is repainted with an animation frame we update x,y,z access to rotate
+
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
 
+  moon.rotation.x += 0.005;
+
+  // controls.update();
+
   renderer.render(scene, camera);
 }
+
 animate();
